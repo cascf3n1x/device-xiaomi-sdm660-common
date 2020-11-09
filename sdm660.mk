6b98e2cb2c19c3d1e523de03a636b6ac9075f294
Michael Lenskiy <lmichael1003@gmail.com>
2020-11-09 13:15:06 +0200

    sdm660-common: Evolution X bringup
    Signed-off-by: Michael Lenskiy <lmichael1003@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -28,7 +28,6 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosip \
     $(LOCAL_PATH)/overlay-system
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
